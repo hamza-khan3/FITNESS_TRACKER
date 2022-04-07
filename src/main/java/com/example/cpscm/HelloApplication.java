c6bef2b895b760813cd6f70d509433c43b3a370f
Eligible_programmer <ravalekhan007@gmail.com>
2022-04-07 00:31:30 -0600

    Setting up project

--- src/main/java/com/example/cpscm/HelloApplication.java
+++ src/main/java/com/example/cpscm/HelloApplication.java
@@ -1,4 +1,5 @@
 package com.example.cpscm;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 620, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/cpscm/HelloController.java
+++ src/main/java/com/example/cpscm/HelloController.java
@@ -1,17 +1,57 @@
 package com.example.cpscm;
-//aa
-import com.example.classes.*;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import com.example.classes.*;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField age;
 
     @FXML
-    protected void onHelloButtonClick() {
+    private TextField cardio;
 
-        welcomeText.setText("Welcome to JavaFX Application!");
+    @FXML
+    private TextField duration;
 
+    @FXML
+    private ToggleGroup ex;
+
+    @FXML
+    private TextField height;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField pace;
+
+    @FXML
+    private TextField weight;
+
+    Alert alert = new Alert(Alert.AlertType.NONE);
+
+    @FXML
+    void savePerson(ActionEvent event) {
+        try {
+            String name_value = name.getText();
+            int age_value = Integer.parseInt(age.getText());
+            double weight_value = Double.parseDouble(weight.getText());
+            double pace_value = Double.parseDouble(pace.getText());
+            double height_value = Double.parseDouble(height.getText());
+            int cardio_value = Integer.parseInt(cardio.getText());
+            double duration_value = Double.parseDouble(duration.getText());
+
+            User user = new User(name_value, age_value, weight_value, pace_value, height_value, cardio_value, duration_value);
+
+        } catch (Exception e) {
+            alert.setAlertType(Alert.AlertType.ERROR);
+            alert.setContentText("Enter correct values for the user information!");
+            alert.show();
+        }
     }
 }
