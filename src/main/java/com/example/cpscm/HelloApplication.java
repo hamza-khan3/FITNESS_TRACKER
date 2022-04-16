a75e69b6c0d30923e40220f801692a86715021af
hamza.khan3 <hamza.khan3@cs.ucalgary.ca>
2022-04-16 02:03:26 +0000

    fixing file names

--- src/main/java/com/example/cpscm/HelloApplication.java
+++ src/main/java/com/example/cpscm/HelloApplication.java
@@ -3,7 +3,7 @@
  * Tutorial: Ravale Khan: T06 and Hamza Khan: T04
  * Date: Friday, April 15th, 2022
  */
-package com.example.cpscm;
+package tracker.project.app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -16,10 +16,10 @@ import java.io.IOException;
  * Making the GUI window.
  */
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 500);
         stage.setTitle("Fitness tracker v420.0");
         stage.setScene(scene);
